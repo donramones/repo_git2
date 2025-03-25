2167b5422570b96708426aead93dfe3eccd6e0f1
donramones <agusrivarolaa00@gmail.com>
2025-03-25 19:11:25 -0300

    agrego variable cursado

--- prueba_git_1.py
+++ prueba_git_1.py
@@ -1,4 +1,5 @@
 nombre="Agu"
 apellido="mrrobot"
 materia="programacion_1"
-print(f"hola soy {nombre} {apellido} alumno de {materia}")
+cursado ="primer cuatrimestre"
+print(f"hola soy {nombre} {apellido} alumno de {materia} del {cursado}")
